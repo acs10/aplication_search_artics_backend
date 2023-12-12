26a93e5a76289833fc579a07aeb308f6e5336e20
Andre Costa Silva <andre.append@gmail.com>
2023-12-11 22:12:11 -0300

    Revert "update: models"
    This reverts commit 1f110d89de4a68327f157b0ea2896fac3dfbc58c.

--- articsprocess/models.py
+++ articsprocess/models.py
@@ -1,37 +1,9 @@
 from django.db import models
 from django.utils.safestring import mark_safe
-from django.contrib.auth.models import User
 # from uuid import uuid4
 
-class UserProfile(models.Model):
-    # body = models.TextField(blank=True, null=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, blank=True, null=True)
-    email = models.CharField(max_length=200, blank=True, null=True)
-    telephone = models.CharField(max_length=20, blank=True, null=True)
-    state = models.CharField(max_length=20, blank=True, null=True)
-    city = models.CharField(max_length=20, blank=True, null=True)
-    country = models.CharField(max_length=20, blank=True, null=True)
-    age = models.IntegerField(blank=True, null=True)
-    occupation = models.CharField(max_length=20, blank=True, null=True)
-    interest = models.CharField(max_length=20, blank=True, null=True)
-    
-class SearchHistory(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    query = models.CharField(max_length=200, blank=True, null=True)
-    sort_by_date = models.CharField(max_length=200, blank=True, null=True)
-    language = models.CharField(max_length=200, blank=True, null=True)
-    period_start = models.CharField(max_length=200, blank=True, null=True)
-    period_end = models.CharField(max_length=200, blank=True, null=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    
-    
-class Article(models.Model):
-    title = models.CharField(max_length=200, blank=True, null=True)
-    authors = models.CharField(max_length=200, blank=True, null=True)
-    link = models.CharField(max_length=200, blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
-    
+class Text(models.Model):
+    body = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.body
